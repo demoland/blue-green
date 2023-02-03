d59b0551b79f888aa27902ad8f0ac65da6a56d5e
Dan Fedick <daniel.fedick@hashicorp.com>
2023-02-03 10:40:37 -0500

    Merge all green deploy files

--- green.tf
+++ green.tf
@@ -1,3 +1,15 @@
+variable "enable_green_env" {
+  description = "Enable green environment"
+  type        = bool
+  default     = true
+}
+
+variable "green_instance_count" {
+  description = "Number of instances in green environment"
+  type        = number
+  default     = 2
+}
+
 resource "aws_instance" "green" {
   count = var.enable_green_env ? var.green_instance_count : 0
 
@@ -35,3 +47,7 @@ resource "aws_lb_target_group_attachment" "green" {
   port             = 80
 }
 
+
+output "tg_green_arn" {
+  value = aws_lb_target_group.green.arn
+}
