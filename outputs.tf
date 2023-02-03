d45944ccc8e85755954b7b4e63047124f3890411
Dan Fedick <daniel.fedick@hashicorp.com>
2023-02-03 10:39:08 -0500

    Enable Green

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,9 @@ output "lb_dns_name" {
   value = aws_lb.app.dns_name
 }
 
-output "tg_arn" {
+output "tg_blue_arn" {
   value = aws_lb_target_group.blue.arn
+}
+output "tg_green_arn" {
+  value = aws_lb_target_group.green.arn
 }
